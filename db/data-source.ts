10138f0f4fa849e6b657bc5474e4db69c94d0a3f
unknown <vomanhhai230720@.com>
2024-03-21 10:38:19 +0700

    first commit

---
+++ db/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '2307',
+  database: 'bazar',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/db/migrations/*{.ts,.js}'],
+  logging: false,
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+dataSource.initialize();
+export default dataSource;
